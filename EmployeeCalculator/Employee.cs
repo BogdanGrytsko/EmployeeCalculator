c8ccaaac6d177a243c6c03cc6f6e3b41d9083a41
Bogdan Hrytsko <bhrytsko@ecovadis.com>
2024-10-09 19:14:17 +0200

    Add project files.

---
+++ EmployeeCalculator/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeCalculator;
+
+public class Employee
+{
+    public long TaxId { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public DateTime JoinDate { get; set; }
+    public int Year { get; set; }
+    public decimal Jan { get; set; }
+    public decimal Feb { get; set; }
+    public decimal Mar { get; set; }
+    public decimal Apr { get; set; }
+    public decimal May { get; set; }
+    public decimal Jun { get; set; }
+    public decimal Jul { get; set; }
+    public decimal Aug { get; set; }
+    public decimal Sep { get; set; }
+    public decimal Oct { get; set; }
+    public decimal Nov { get; set; }
+    public decimal Dec { get; set; }
+
+    public decimal YearlySum()
+    {
+        return Jan + Feb + Mar + Apr + May + Jun + Jul + Aug + Sep + Oct + Nov + Dec;
+    }
+}
---
+++ EmployeeCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeCalculator;
+
+var lines = File.ReadLines("SomeData.csv").ToList();
+
+var employees = new List<Employee>();
+foreach (var line in lines.Skip(1))
+{
+    var splitLine = line.Split(';');
+    var employee = new Employee
+    {
+        TaxId = long.Parse(splitLine[0]),
+        Name = splitLine[1],
+        LastName = splitLine[2],
+        BirthDate = DateTime.Parse(splitLine[3]),
+        JoinDate = DateTime.Parse(splitLine[4]),
+        Year = int.Parse(splitLine[5]),
+        Jan = decimal.Parse(splitLine[6]),
+        Feb = decimal.Parse(splitLine[7]),
+        Mar = decimal.Parse(splitLine[8]),
+        Apr = decimal.Parse(splitLine[9]),
+        May = decimal.Parse(splitLine[10]),
+        Jun = decimal.Parse(splitLine[11]),
+        Jul = decimal.Parse(splitLine[12]),
+        Aug = decimal.Parse(splitLine[13]),
+        Sep = decimal.Parse(splitLine[14]),
+        Oct = decimal.Parse(splitLine[15]),
+        Nov = decimal.Parse(splitLine[16]),
+        Dec = decimal.Parse(splitLine[17]),
+    };
+    employees.Add(employee);
+}
+
+var outputLines = new List<string> { "TaxId;YearlySum" };
+foreach (var employee in employees)
+{
+    var line = $"{employee.TaxId};{employee.YearlySum()}";
+    outputLines.Add(line);
+}
+
+File.WriteAllLines("Result.csv", outputLines);
