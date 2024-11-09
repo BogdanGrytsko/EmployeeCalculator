6e1c75a55705cb972dcf2af2f72dc77081490921
Bogdan Hrytsko <bhrytsko@ecovadis.com>
2024-11-09 19:54:14 +0100

    Interview questions 1. Monty Hall problem

---
+++ EmployeeCalculator/MontyHallProblem.cs
@@ -0,0 +1,46 @@
+﻿namespace EmployeeCalculator;
+
+public class MontyHallProblem
+{
+    public static void Simultate()
+    {
+        var numberOfTries = 10000;
+        var wins = 0;
+        var losses = 0;
+        for (int i = 0; i < numberOfTries; i++)
+        {
+            if (SimulateOneTry())
+                wins++;
+            else
+                losses++;
+        }
+        Console.WriteLine($"Wins: {wins}, Losses: {losses}, Win ratio: {(double)wins / numberOfTries}");
+    }
+
+    private static bool SimulateOneTry()
+    {
+        var rnd = new Random();
+        var doors = new List<int> { 0, 0, 0 };
+        var prizeDoor = rnd.Next(3);
+        doors[prizeDoor] = 1;
+        // Player picks a door
+        var playerDoorChoiceInitial = rnd.Next(3);
+        var hostCanReveal = new List<int>();
+        for (int i = 0; i < doors.Count; i++)
+        {
+            if (doors[i] == 1) continue;
+            if (i == playerDoorChoiceInitial) continue;
+            hostCanReveal.Add(i);
+        }
+
+        var revealDoor = hostCanReveal[rnd.Next(hostCanReveal.Count)];
+
+        //option 1: stay with the door
+        //option 2: choose another door
+        var allDoorsIndexes = new List<int> { 0, 1, 2 };
+        var playerDoorChoiceChanged = allDoorsIndexes.Except(new List<int> { playerDoorChoiceInitial, revealDoor }).Single();
+
+        var result = doors[playerDoorChoiceChanged] == 1;
+        return result;
+    }
+}
--- EmployeeCalculator/Program.cs
+++ EmployeeCalculator/Program.cs
@@ -2,19 +2,20 @@
 using EmployeeCalculator;
 using Microsoft.EntityFrameworkCore;
 
-var options = new DbContextOptionsBuilder<EmployeeContext>()
-                        .UseSqlServer("Server=.;Database=EmployeeDB;trusted_connection=true;TrustServerCertificate=True")
-                        .Options;
-var context = new EmployeeContext(options);
-var manager = new EmployeeManager();
-manager.ReadFromDatabase(context);
-//manager.ReadFromDisk("SomeData.csv");
-//await manager.SaveToDatabase(context);
+MontyHallProblem.Simultate();
+//var options = new DbContextOptionsBuilder<EmployeeContext>()
+//                        .UseSqlServer("Server=.;Database=EmployeeDB;trusted_connection=true;TrustServerCertificate=True")
+//                        .Options;
+//var context = new EmployeeContext(options);
+//var manager = new EmployeeManager();
+//manager.ReadFromDatabase(context);
+////manager.ReadFromDisk("SomeData.csv");
+////await manager.SaveToDatabase(context);
 
-var yearlySumBytes = manager.GetYearlySumFile();
-File.WriteAllBytes("Output.csv", yearlySumBytes);
+//var yearlySumBytes = manager.GetYearlySumFile();
+//File.WriteAllBytes("Output.csv", yearlySumBytes);
 
-var topEarners = manager.TopN(5);
-File.WriteAllBytes("TopEarners.csv", topEarners);
-var bottomEarners = manager.BottomN(5);
-File.WriteAllBytes("BottomEarners.csv", bottomEarners);
+//var topEarners = manager.TopN(5);
+//File.WriteAllBytes("TopEarners.csv", topEarners);
+//var bottomEarners = manager.BottomN(5);
+//File.WriteAllBytes("BottomEarners.csv", bottomEarners);
